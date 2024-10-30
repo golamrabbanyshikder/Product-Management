c63405b8d94f652cce75ca22a527052d718c3a54
golam_rabbany <api@ctrends-software.com>
2024-10-30 20:43:07 +0600

    Change

--- src/main/java/com/product_management/ProductManagementApplication.java
+++ src/main/java/com/product_management/ProductManagementApplication.java
@@ -8,7 +8,6 @@ public class ProductManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManagementApplication.class, args);
-		System.out.println("test");
 	}
 
 }
